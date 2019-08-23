d65682deb5e6a45e28522875c21240394bee2603
Gabriel Poça <gabrielpoca@gmail.com>
2019-08-23 10:09:53 +0100

    remove unused code

--- src/index.js
+++ src/index.js
@@ -22,14 +22,4 @@ exports.activate = context => {
   subscriptions.push(
     listManager.registerList(new Notifications(axiosInstance, workspace))
   );
-
-  context.subscriptions.push(
-    commands.registerCommand("octobox.list", async () => {
-      const res = await axiosInstance.get("notifications.json");
-
-      console.log(res.data);
-      res.data.notifications.map(notif => console.log(notif.subject.title));
-      // FINISH THIS PART
-    })
-  );
 };
