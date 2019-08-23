795564c1083ca06286253792e87c051a2b14b64b
Gabriel Poça <gabrielpoca@gmail.com>
2019-08-23 10:10:00 +0100

    better labels and syntax highlight

--- src/notifications.js
+++ src/notifications.js
@@ -1,12 +1,13 @@
 const child_process = require("child_process");
 
 class Notifications {
-  constructor(axios, workspace) {
+  constructor(axios, workspace, nvim) {
     this.name = "octobox";
     this.description = "octobox notifications list";
     this.defaultAction = "open";
     this.actions = [];
     this.axios = axios;
+    this.nvim = nvim;
     this.workspace = workspace;
 
     this.actions = [
@@ -45,10 +46,12 @@ class Notifications {
   }
 
   async loadItems(_context) {
-    const result = await this.axios.get("notifications.json");
+    const result = await this.axios.get("notifications.json?per_page=100");
 
     return result.data.notifications
       .filter(({ archived }) => !archived)
+      .map(notif => ({ ...notif, updated_at: new Date(notif.updated_at) }))
+      .sort(({ updated_at }) => updated_at)
       .map(notif => {
         console.log(notif);
         return { label: this.buildLabel(notif), data: notif };
@@ -56,7 +59,33 @@ class Notifications {
   }
 
   buildLabel({ subject, unread, reason }) {
-    return `${unread ? "unread" : "read"} ${subject.title} ${reason}`;
+    return `${unread ? "unread" : "read"} ${
+      subject.type === "PullRequest" ? "PR" : subject.type
+    } ${subject.state} ${subject.title} ${reason}`;
+  }
+
+  doHighlight() {
+    const { nvim } = this.workspace;
+
+    nvim.pauseNotification();
+    nvim.command(
+      `syntax keyword CocOctoboxMerged merged contained containedin=CocOctoboxLine`,
+      true
+    );
+    nvim.command(
+      `syntax keyword CocOctoboxOpen open contained containedin=CocOctoboxLine`,
+      true
+    );
+    nvim.command(
+      `syntax match CocOctoboxRead /^read\ .*/ contained containedin=CocOctoboxLine`,
+      true
+    );
+    nvim.command("highlight default link CocOctoboxRead Comment", true);
+    nvim.command("highlight default link CocOctoboxMerged Constant", true);
+    nvim.command("highlight default link CocOctoboxOpen String", true);
+    nvim.resumeNotification().catch(_e => {
+      // noop
+    });
   }
 }
 
